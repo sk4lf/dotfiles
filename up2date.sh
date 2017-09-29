5b7c4e96a79a6143f5c40328135c9a0d5065f1d1
Oleksii Kolodiazhnyi <akolodyazhnyy@mirantis.com>
2017-09-29 10:24:31 +0300

    improved logging and output with mode details and colors

--- up2date.sh
+++ up2date.sh
@@ -6,6 +6,21 @@
 #
 #--------------------------------------------------
 
+# Colors
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+NC='\033[0m' # No Color
+
 # Variables
 
 HOMEDIR=/Users/kl/
@@ -16,9 +31,9 @@ LOG=$LOGDIR$LOGFILE
 
 # Print date
 
-echo ----------------------------
-date
-echo ----------------------------
+echo "${GREEN}----------------------------${NC}" | tee -a $LOG
+date | tee -a $LOG
+echo "${GREEN}----------------------------${NC}" | tee -a $LOG
 
 # Log collection
 
@@ -26,8 +41,11 @@ mkdir -p $LOGDIR
 touch $LOG
 
 # Brew
+echo "${PURPLE}Brew Update${NC}" | tee -a $LOG
 brew update | tee -a $LOG && 
+echo "${PURPLE}Brew Upgrade${NC}" | tee -a $LOG
 brew upgrade | tee -a $LOG
 
 #oh-my-zsh
+echo "${PURPLE}oh-my-zsh${NC}" | tee -a $LOG
 sh $ZSHDIR/tools/upgrade.sh | tee -a $LOG
